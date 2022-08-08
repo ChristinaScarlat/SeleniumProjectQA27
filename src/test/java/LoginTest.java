c58e4f9b15860be47d1105233e8af4812bf3aa4f
ChristinaS <alexandra.christina@yahoo.com>
2022-08-08 20:05:12 +0300

    FirstSeleniumProjectQA27Lab2-Tema2

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,19 +1,100 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
 
 public class LoginTest {
+    private WebDriver driver;
 
-    public void validLoginTest(){
+    @Before
+    public void initDriver(){
         System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
+        driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("http://testfasttrackit.info/selenium-test");
-        driver.findElement(By.cssSelector("#header > div > div.skip-links > div > a > span.label")).click();
-        driver.findElement(By.cssSelector("#header-account > div > ul > li.last > a")).click();
+    }
+
+    @Test
+    public void validLoginTest(){
+
+        driver.findElement(By.cssSelector(".skip-account .label")).click();
+        driver.findElement(By.cssSelector("[title = 'Log In']")).click();
         driver.findElement(By.id("email")).sendKeys("alexandra.christina@yahoo.com");
         driver.findElement(By.id("pass")).sendKeys("1234567");
         driver.findElement(By.id("send2")).click();
-  //      driver.quit();
+
     }
+    @Test
+    public void changeLanguageTest(){
+//        select-language
+        Select selectLanguageDropdown = new Select(driver.findElement(By.id("select-language")));
+        selectLanguageDropdown.selectByVisibleText("German");
+
+    }
+
+    @Test
+    public void hoverOverCategoryTest(){
+        Actions action = new Actions(driver);
+        WebElement womenCategory = driver.findElement(By.cssSelector("#nav > ol > li.level0.nav-1.first.parent > a"));
+        action.moveToElement(womenCategory).perform();
+        WebElement viewAll = driver.findElement(By.cssSelector("#nav > ol > li.level0.nav-1.first.parent > ul > li.level1.view-all > a"));
+        viewAll.click();
+
+
+    }
+
+
+    @Test
+    public void searchButtonTest(){
+
+       // driver.findElement(By.id("search")).click();
+        driver.findElement(By.cssSelector(".input-box button")).click();
+
+    }
+    @Test
+    public void addElementCartTest(){
+        driver.findElement(By.cssSelector("li.level0.nav-6 a.level0")).click();
+        driver.findElement(By.cssSelector("li.item.last:nth-last-of-type(2) div.product-info h2.product-name a")).click();
+        driver.findElement(By.id("swatch20")).click();
+        driver.findElement(By.cssSelector("div.add-to-cart-buttons button")).click();
+
+    }
+    @Test
+    public void removeItemCartTest(){
+        driver.findElement(By.cssSelector("li.level0.nav-6 a.level0")).click();
+        driver.findElement(By.cssSelector("li.item.last:nth-last-of-type(2) div.product-info h2.product-name a")).click();
+        driver.findElement(By.id("swatch20")).click();
+        driver.findElement(By.cssSelector("div.add-to-cart-buttons button")).click();
+        driver.findElement(By.cssSelector("td.a-center.product-cart-remove.last a")).click();
+}
+    @Test
+    public void checkoutTest(){
+        driver.findElement(By.cssSelector("li.level0.nav-6 a.level0")).click();
+        driver.findElement(By.cssSelector("li.item.last:nth-last-of-type(2) div.product-info h2.product-name a")).click();
+        driver.findElement(By.id("swatch20")).click();
+        driver.findElement(By.cssSelector("div.add-to-cart-buttons button")).click();
+        driver.findElement(By.cssSelector("div.header-minicart span.label")).click();
+        driver.findElement(By.cssSelector("div.minicart-actions a.cart-link")).click();
+        driver.findElement(By.cssSelector("li.method-checkout-cart-methods-onepage-bottom button.btn-proceed-checkout.btn-checkout ")).click();
+    }
+
+    @Test
+    public void selectPosition(){
+        driver.findElement(By.cssSelector("li.level0.nav-5.parent a.level0.has-children")).click();
+        driver.findElement(By.cssSelector(".sort-by select ")).click();
+    }
+
+
+
+
+    @After
+    public void closeDriver(){
+        driver.quit();
+    }
+
 }
--- src/test/java/RegisterTest.java
+++ src/test/java/RegisterTest.java
@@ -1,14 +1,24 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class RegisterTest {
+    private WebDriver driver;
 
+    @Before
+    public void initDriver(){
+        System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("http://testfasttrackit.info/selenium-test");
+    }
+
+        @Test
         public void validRegisterTest(){
-            System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
-            WebDriver driver = new ChromeDriver();
-            driver.manage().window().maximize();
-            driver.get("http://testfasttrackit.info/selenium-test");
+
             driver.findElement(By.cssSelector("#header > div > div.skip-links > div > a > span.label")).click();
             driver.findElement(By.cssSelector("#header-account > div > ul > li:nth-child(5) > a")).click();
             driver.findElement(By.id("firstname")).sendKeys("Christina");
@@ -18,10 +28,12 @@ public class RegisterTest {
             driver.findElement(By.id("confirmation")).sendKeys("1234567");
             driver.findElement(By.cssSelector("#is_subscribed")).click();
             driver.findElement(By.cssSelector("#form-validate > div.buttons-set > button")).click();
-            driver.quit();
-
 
 
         }
+    @After
+    public void closeDriver(){
+        driver.quit();
+    }
     }
 
